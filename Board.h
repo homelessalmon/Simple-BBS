763134e561a2d3a1b1750d074382199adbd6cd9a
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-05-22 23:04:36 +0800

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,5 +1,15 @@
 #pragma once
+#include<string>
+#include<vector>
+class Post
+{
+  string title;
+  string content;
+  vector<string> comment;
+};
 class Board
 {
+  Post post;
+  int permission
 };
 
