08dbbd12ecbd7195ff83b8625f2eb2671e8cb61c
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-05-23 01:14:01 +0800

    Update Viewer.cpp

--- Viewer.cpp
+++ Viewer.cpp
@@ -11,5 +11,24 @@ void Viewer::login(string& username, string& password)
 	cout << "\nUsername: ";
 	cin >> username;
 	cout << "Password: ";
-	cin >> password;
+    char pass[32];
+    char tmp;
+    int i = 0;
+    for (i = 0;;) {
+        tmp = _getch();
+        if ((tmp >= 'a' && tmp <= 'z') || (tmp >= 'A' && tmp <= 'Z') || (tmp >= '0' && tmp <= '9')) {
+            pass[i] = tmp;
+            ++i;
+            cout << "*";
+        }
+        if (tmp == '\b' && i >= 1) { //if user typed backspace (i should be greater than 1)
+            cout << "\b \b";//rub the character behind the cursor
+            --i;
+        }
+        if (tmp == '\r') { //if enter is pressed
+            pass[i] = '\0';//null means end of string
+            break;
+        }
+    }
+    password = pass;
 }
