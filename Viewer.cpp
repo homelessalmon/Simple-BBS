70ab6ccd487044f759e72d34ab42ec974ab8b005
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-05-23 00:42:03 +0800

    Update Viewer.cpp

--- Viewer.cpp
+++ Viewer.cpp
@@ -1,1 +1,15 @@
 #include "Viewer.h"
+
+void Viewer::login(string& username, string& password)
+{
+	cout << "  _                _" << endl;
+	cout << " | |  ___    __ _ (_) _ __" << endl;
+	cout << " | | / _ \\ /  _  || ||  _ \\" << endl;
+	cout << " | || (_) || (_| || || | | |" << endl;
+	cout << " |_| \\___/ \\__,  ||_||_| |_|" << endl;
+	cout << "           |___ /" << endl;
+	cout << "\nUsername: ";
+	cin >> username;
+	cout << "Password: ";
+	cin >> password;
+}
