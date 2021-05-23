5b22eeb122e1cc6e87a1f0230c3d84932128d4e5
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-05-23 23:37:09 +0800

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,14 +1,19 @@
 #pragma once
 #include<string>
 #include<vector>
+
+using namespace std;
+
 class Post
 {
+public:
   vector<string> title;
-  vector<string> string content;
+  vector<string> content;
   vector<string> comment;
 };
 class Board
 {
+public:
   vector<int> post_id;
   int permission;
   string board_name;
