b2de236081753a12cb387ff3bd19c63f99ec1885
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-05-24 00:58:11 +0800

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,1 +1,14 @@
 #include "User.h"
+
+Adiministrator::Adiministrator(string user_name, string user_password)
+{
+	ifstream fin("users/count.txt");
+	int count; fin >> count; fin.close();
+
+	ofstream fout("users/count.txt", ios::out || ios::trunc); fout << count + 1; fout.flush(); fout.close();
+	system(("cd.>users/" + to_string(count + 1) + ".txt").c_str());
+
+	fout.open("users/" + to_string(count + 1) + ".txt", ios::out || ios::trunc);
+	fout << user_name << endl << user_password << endl << 2 << endl;
+	fout.flush(); fout.close();
+}
