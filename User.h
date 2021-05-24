09e0592b399a5f44b8801483cc4449c75d9d852d
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-05-24 11:00:41 +0800

    Update User.h

--- User.h
+++ User.h
@@ -10,6 +10,7 @@ class User
 public:
 	vector <int> postsID;	// 紀錄建立過的post
 	int Permission_level = -1;	// 權限等級
+	int user_id = -1;
 	// Other infos, e.x. comment_nums, post_num, ….
 
 	User(){}
@@ -21,17 +22,18 @@ class Adiministrator : public User
 public:
 	Adiministrator(string user_name, string user_password);
 	void add_board();
-	void add_post();
+	void add_post(int current_board);
 };
 
 class Member : public User
 {
 public:
 	Member(string user_name, string user_password);
-	void add_post();
+	void add_post(int current_board);
 };
 
 class Guest : public User
 {
 public:
+	Guest() :User() {}
 };
