1b3a930eaf0d4eba071b757eb8bf4cb10364bd1c
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-06-02 03:46:11 +0800

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -10,6 +10,6 @@ class Viewer
 {
 public:
 	int menu();
-	int login(string&, string&);
-	int signup(string&, string&);
+	int login(string&, string&); //0:back to menu 1: end successfully 
+	int signup(string&, string&); //0:back to menu 1: end successfully 
 };
