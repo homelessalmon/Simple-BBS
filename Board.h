fb18523a086a51671d036d0da46e6f1fd49f6f3b
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-06-04 23:50:12 +0800

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,21 +1,42 @@
 #pragma once
 #include<string>
 #include<vector>
+#include<fstream>
+#include<sstream>
 
 using namespace std;
+
+const string REMOVE = "-1402834659875121315.1";
+
+class Comment
+{
+public:
+	string name;
+	string com;//reason if removed
+	bool is_removed = false;
+
+	Comment(string _name, string _com) :name(_name), com(_com) { }
+	friend vector<Comment> load_comment(int post_id);
+};
 
 class Post
 {
 public:
   vector<string> title;
   vector<string> content;
-  vector<string> comment;
+  vector<Comment> comments;
+  bool is_removed = false;
+
+  Post(vector<string> _title, vector<string> _content, vector<Comment> _comments);
+  void load_post(int post_id);
 };
+
 class Board
 {
 public:
   vector<int> post_id;
-  string board_name;
+  string board_name;//reason if removed;
+  bool is_removed = false;
 
   Board(vector<int> _post_id, string board_name);
 };
