ac0e51e6d1818d1c53697a5e5fd67501f38e47b3
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-06-05 17:18:28 +0800

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -1,15 +1,78 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+#include <vector>
 #include <conio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
 
 class Viewer
 {
 public:
-	int menu();
-	int login(string&, string&); //0:back to menu 1: end successfully 
-	int signup(string&, string&); //0:back to menu 1: end successfully 
+	int menu1(); //0: Exit; 1: Log in: 2: Sign up: 3: Guest
+	int menu2(); //0: Logout; 1: View Boards: 2: Mailbox
+	int login(string&, string&); //0: back to menu; 1: end successfully 
+	int signup(string&, string&); //0: back to menu; 1: end successfully
+	void board_select(); //-1: back; -2: menu; -3: add board(admin); 0~n: board ID
+	void post_select();
+};
+
+class Button
+{
+public:
+	Button() {};
+	Button(string t, sf::Vector2f size, int charSize, sf::Color bgColor, sf::Color textColor) {
+		text.setString(t);
+		text.setFillColor(textColor);
+		text.setCharacterSize(charSize);
+		button.setSize(size);
+		button.setFillColor(bgColor);
+	}
+
+	void setFont(sf::Font& font) {
+		text.setFont(font);
+	}
+
+	void setText(string txt) {
+		this->text.setString(txt);
+	}
+
+	void setBgColor(sf::Color color) {
+		button.setFillColor(color);
+	}
+
+	void setTxtColor(sf::Color color) {
+		text.setFillColor(color);
+	}
+
+	void setPos(sf::Vector2f pos) {
+		button.setPosition(pos);
+		float x = (pos.x + button.getGlobalBounds().width / 3) - (text.getGlobalBounds().width / 4);
+		float y = (pos.y + button.getGlobalBounds().height / 4) - (text.getGlobalBounds().height / 4);
+		text.setPosition({ x, y });
+	}
+
+	void drawTo(sf::RenderWindow& window) {
+		window.draw(button);
+		window.draw(text);
+	}
+
+	bool isMouseOver(sf::RenderWindow& window) {
+		float mouseX = sf::Mouse::getPosition(window).x;
+		float mouseY = sf::Mouse::getPosition(window).y;
+		float buttonX = button.getPosition().x;
+		float buttonY = button.getPosition().y;
+		float buttonW = button.getPosition().x + button.getLocalBounds().width;
+		float buttonH = button.getPosition().y + button.getLocalBounds().height;
+		if (mouseX < buttonW && mouseX > buttonX && mouseY < buttonH && mouseY > buttonY) {
+			return true;
+		}
+		return false;
+	}
+private:
+	sf::RectangleShape button;
+	sf::Text text;
 };
