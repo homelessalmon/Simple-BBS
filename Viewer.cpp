8909908152e1bcedbea37d05197142bc00cd977d
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-06-05 17:18:01 +0800

    Update Viewer.cpp

--- Viewer.cpp
+++ Viewer.cpp
@@ -1,6 +1,6 @@
 #include "Viewer.h"
 
-int Viewer::menu()
+int Viewer::menu1()
 {
     cout << " _ __ ___    ___  _ __   _   _" << endl;
     cout << "| '_ ` _ \\  / _ \\| '_ \\ | | | |" << endl;
@@ -112,4 +112,78 @@ int Viewer::signup(string& username, string& password)
     }
     password = pass;
     return 1;
+}
+
+int Viewer::menu2()
+{
+    sf::Font font;
+    font.loadFromFile("consola.ttf");
+    sf::Text title;
+    title.setFont(font);
+    title.setFillColor(sf::Color::Red);
+    title.setCharacterSize(100);
+    title.setString("BBS");
+    title.setPosition({ 255, 50 });
+
+    sf::RenderWindow window(sf::VideoMode(700, 800), "Welcome!", sf::Style::Default ^ sf::Style::Resize);
+
+    Button board("View Boards", { 400, 70 }, 50, sf::Color::Black, sf::Color::White);
+    board.setFont(font);
+    board.setPos({ 150, 220 });
+    Button mailbox("Mailbox", { 400, 70 }, 50, sf::Color::Black, sf::Color::White);
+    mailbox.setFont(font);
+    mailbox.setPos({ 150, 320 });
+    Button logout("Log out", { 400, 70 }, 50, sf::Color::Black, sf::Color::White);
+    logout.setFont(font);
+    logout.setPos({ 150, 420 });
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::MouseMoved:
+                if (board.isMouseOver(window)) {
+                    board.setBgColor(sf::Color::White);
+                    board.setTxtColor(sf::Color::Black);
+                }
+                else if (mailbox.isMouseOver(window)) {
+                    mailbox.setBgColor(sf::Color::White);
+                    mailbox.setTxtColor(sf::Color::Black);
+                }
+                else if (logout.isMouseOver(window)) {
+                    logout.setBgColor(sf::Color::White);
+                    logout.setTxtColor(sf::Color::Black);
+                }
+                else {
+                    board.setBgColor(sf::Color::Black);
+                    board.setTxtColor(sf::Color::White);
+                    mailbox.setBgColor(sf::Color::Black);
+                    mailbox.setTxtColor(sf::Color::White);
+                    logout.setBgColor(sf::Color::Black);
+                    logout.setTxtColor(sf::Color::White);
+                }
+                break;
+            case sf::Event::MouseButtonPressed:
+                if (logout.isMouseOver(window)) {
+                    return 0;
+                }
+                if (board.isMouseOver(window)) {
+                    return 1;
+                }
+                if (mailbox.isMouseOver(window)) {
+                    return 2;
+                }
+                break;
+            }
+        }
+
+        board.drawTo(window);
+        mailbox.drawTo(window);
+        logout.drawTo(window);
+        window.draw(title);
+        window.display();
+    }
 }
