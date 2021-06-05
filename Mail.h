22965dd0d67e760c74605a04815059959c04c1fc
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-06-06 00:35:11 +0800

    Update Mail.h

--- Mail.h
+++ Mail.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<string>
 #include<vector>
+#include<fstream>
 
 using namespace std;
 
@@ -10,9 +11,9 @@ public:
 	string from;
 	vector<string> content;
 
-	Mail();
-	Mail(string _from, vector<string> content);
-
-	friend bool send_mail(string receiver_user_name, int sender_user_id);//return false if receiver not found
+	Mail() { }
+	Mail(string _from, vector<string> _content);
+	
 };
 
+bool send_mail(string receiver_user_name, int sender_user_id);//return false if receiver not found
