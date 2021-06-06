98ce6db33addc7a2c2a2245c8ceb61f6ef083841
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-06-07 02:59:24 +0800

    Update Mail.h

--- Mail.h
+++ Mail.h
@@ -9,11 +9,12 @@ class Mail
 {
 public:
 	string from;
+	string title;
 	vector<string> content;
 
 	Mail() { }
-	Mail(string _from, vector<string> _content);
+	Mail(string _from, string _title, vector<string> _content);
 	
 };
 
-bool send_mail(string receiver_user_name, int sender_user_id);//return false if receiver not found
+bool send_mail(string receiver_user_name, int sender_user_id, string title);//return false if receiver not found
