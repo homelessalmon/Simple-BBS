5a0dd295ae12c7ce6898de705119a4daec835f4a
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-06-07 06:29:10 +0800

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -29,7 +29,7 @@ public:
 	int board_select(vector<Board>, int permission_lv); //-1.back; -2.logout; -3.add board(admin); -4.del board(admin); 0~n.board ID
 	int board_add(vector<Board>, string&); // -1.back(no add); 1.add successfully
 	int board_delete(vector<Board>); //-1.back(no delete); 0~n.board to be deleted
-	int post_select(Board); //-1.back; -2.logout; -3.add; 0~n.post ID
+	int post_select(Board, int permission_lv); //-1.back; -2.logout; -3.add; 0~n.post ID
 	int view_post(vector<Post> posts, string author, int authorID, int postID, int boardID, int userID, int permission_lv); //-1.back; -2.logout; -3.view comment; -4.edit; -5.del_post;
 	int view_comment(vector<Post> posts, int postID, int userID, int permission_lv); //-1.back; -100.leave comment; 0~n.comment to be deleted
 	int window_txtbox(string title, string info, string& input, int limit, float posX, float posY); //to input one non-empty string
