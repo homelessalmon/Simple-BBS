41d751b0cfb623fda9c8c031c89d1235184f2cb1
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-06-06 22:45:53 +0800

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -6,7 +6,7 @@ Board::Board(vector<int> _post_id, string _board_name)
 	board_name = _board_name;
 }
 
-Post::Post(vector<string> _title, vector<string> _content, vector<Comment> _comments)
+Post::Post(string _title, vector<string> _content, vector<Comment> _comments)
 {
 	title = _title;
 	content = _content;
@@ -19,9 +19,9 @@ vector<Comment> load_comment(int post_id)
 	string name, com;
 	stringstream ss;
 	vector<Comment> list;
-	while (getline(fin,name))
+	while (getline(fin, name))
 	{
-		ss.str(""); 
+		ss.str("");
 		ss.clear();
 
 		ss << name;
@@ -38,20 +38,36 @@ vector<Comment> load_comment(int post_id)
 	return list;
 }
 
+bool operator>(pair<string, int> left, pair<string, int> right)
+{
+	if (right.second > left.second)return false;
+	else
+	{
+		int ll = left.first.length(), lr = right.first.length();
+		int minl = ll < lr ? ll : lr;
+		for (int i = 0; i < minl; i++)
+		{
+			if (left.first[i] > right.first[i])return true;
+		}
+		if (ll >= lr)return true;
+		return false;
+	}
+}
+
 void Post::load_post(int post_id)
 {
 	ifstream fin("posts/" + to_string(post_id) + "title.txt");
-	string tmp;
-	while (getline(fin, tmp)) { title.push_back(tmp); }
+	getline(fin, title);
 	fin.close();
 
+	string tmp;
 	fin.open("posts/" + to_string(post_id) + "content.txt");
 	while (getline(fin, tmp)) { content.push_back(tmp); }
 	fin.close();
 
 	comments = load_comment(post_id);
 
-	if (title[0] == REMOVE)
+	if (title == REMOVE)
 	{
 		is_removed = true;
 	}
