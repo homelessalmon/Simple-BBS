46ab63ea88a6be6187303a3f6814c06ee8183d87
shinku2846 <shinku2846@gmail.com>
2021-06-07 02:53:45 +0800

    Update BoardManager.h

--- BoardManager.h
+++ BoardManager.h
@@ -5,30 +5,39 @@
 
 using namespace std;
 
-enum BoardState
-{
-    MENU,
-    SELECT_BOARD,
-    BOARD,
-    POST
+enum BoardState {
+	MENU,
+	LOGIN,
+	SIGNUP,
+	MENU_PERSONAL,
+	MAIL,
+	SELECT_BOARD,
+	ADD_BOARD,
+	DELETE_BOARD,
+	BOARD,
+	POST,
+	EDIT_BOARD,
+	COMMENT
 };
 
 
-class BoardManager
-{
+class BoardManager {
 public:
-    vector <User*> users;
-    vector <Board> boards;
-    int current_user = -1;
-    int current_board = -1;
-    int current_post = -1;
+	vector <User*> users;
+	vector <Board> boards;
+	int current_user = -1;
+	int current_board = -1;
+	int current_post = -1;
 
-    BoardState state;
-    Viewer viewer;
+	BoardState state;
+	Viewer viewer;
 
-    void load_user();
-    void load_board();
-    void load_mail();//bound to current users,after calling this function,can use vector users[current_user]->mail
-    vector<pair<string, string>> return_name_and_password();
-    string return_post_author(int post_id);
+	void exe();
+	void load_user();
+	void load_board();
+	void get_user_id(string account);
+	void load_mail();//bound to current users,after calling this function,can use vector users[current_user]->mail
+	vector<pair<string, string>> return_name_and_password();
+	string return_post_author(int post_id);
+	int post_id_to_index(int post_id, vector<Board> boards, int board_id);
 };
